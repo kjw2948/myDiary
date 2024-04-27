01772a36748ae257f664dfb6d9da471f53bf797f
kjw2948 <kjw970313@cau.ac.kr>
2024-04-27 20:26:27 +0900

    [update] memo 저장소 로직 추가

--- src/main/java/project/WeatherDiary/domain/Memo.java
+++ src/main/java/project/WeatherDiary/domain/Memo.java
@@ -1,5 +1,11 @@
 package project.WeatherDiary.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Memo {
     private int id;
     private String text;
